92bd9210b56d3788be986a4a0fd8a93974c9a205
Sara Vieira <s.cvieira@sapo.pt>
2021-01-13 19:18:18 +0000

    applied tail recursion to fibonacci

--- piscine/C05/ex04/ft_fibonacci.c
+++ piscine/C05/ex04/ft_fibonacci.c
@@ -6,17 +6,22 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 18:27:33 by svieira           #+#    #+#             */
-/*   Updated: 2021/01/12 18:48:58 by svieira          ###   ########.fr       */
+/*   Updated: 2021/01/13 19:15:58 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft_tail_fib(int index, int curr, int next)
+{
+	if (index == 0)
+		return (curr);
+	if (index == 1)
+		return (next);
+	return (ft_tail_fib(index - 1, next, curr + next));
+}
 
 int	ft_fibonacci(int index)
 {
 	if (index < 0)
 		return (-1);
-	if (index == 0)
-		return (0);
-	if (index == 1)
-		return (1);
-	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+	return (ft_tail_fib(index, 0, 1));
 }
