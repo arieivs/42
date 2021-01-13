6b682e8e9ddb62006fd17b5ec1a49899ae176fad
Sara Vieira <s.cvieira@sapo.pt>
2021-01-13 18:56:28 +0000

    changed recursive factorial to use tail recursion instead

--- piscine/C05/ex01/ft_recursive_factorial.c
+++ piscine/C05/ex01/ft_recursive_factorial.c
@@ -6,15 +6,20 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 17:49:06 by svieira           #+#    #+#             */
-/*   Updated: 2021/01/12 18:05:53 by svieira          ###   ########.fr       */
+/*   Updated: 2021/01/13 18:53:54 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft_tail_rec_fact(int n, int acc)
+{
+	if (n <= 1)
+		return (acc);
+	return (ft_tail_rec_fact(n - 1, n * acc));
+}
 
 int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
 		return (0);
-	if (nb <= 1)
-		return (1);
-	return (nb * ft_recursive_factorial(nb - 1));
+	return (ft_tail_rec_fact(nb, 1));
 }
