6c474745dc4662bbceabfacbad2d41fef9676d2d
Sara Vieira <s.cvieira@sapo.pt>
2021-01-13 19:07:10 +0000

    applied tail recursion to recursive power

--- piscine/C05/ex03/ft_recursive_power.c
+++ piscine/C05/ex03/ft_recursive_power.c
@@ -6,15 +6,20 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 18:11:26 by svieira           #+#    #+#             */
-/*   Updated: 2021/01/12 18:20:18 by svieira          ###   ########.fr       */
+/*   Updated: 2021/01/13 19:05:31 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft_tail_rec_power(int nb, int power, int acc)
+{
+	if (power == 0)
+		return (acc);
+	return (ft_tail_rec_power(nb, power - 1, nb * acc));
+}
 
 int	ft_recursive_power(int nb, int power)
 {
 	if (power < 0)
 		return (0);
-	if (power == 0)
-		return (1);
-	return (nb * ft_recursive_power(nb, power - 1));
+	return (ft_tail_rec_power(nb, power, 1));
 }
