a742a0ba9caa658b00823dd923d4f71079fce302
Sara Vieira <s.cvieira@sapo.pt>
2021-01-18 12:11:26 +0000

    putstr_non_printable and main from autotest. not working for chars between 128 and 255

---
+++ piscine/C02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/18 10:37:32 by svieira           #+#    #+#             */
+/*   Updated: 2021/01/18 12:10:12 by svieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		is_non_printable(char c)
+{
+	return (c <= 31 || c >= 127);
+}
+
+void	ft_puthexa(unsigned int dec)
+{
+	char hexa1;
+	char hexa2;
+
+	hexa1 = "0123456789abcdef"[dec / 16];
+	hexa2 = "0123456789abcdef"[dec % 16];
+	write(1, "\\", 1);
+	write(1, &hexa1, 1);
+	write(1, &hexa2, 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	unsigned int	dec;
+
+	while (*str)
+	{
+		if (is_non_printable(*str))
+		{
+			dec = *str;
+			ft_puthexa(dec);
+		}
+		else
+			write(1, str, 1);
+		str++;
+	}
+}
---
+++ piscine/C02/ex11/main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_putstr_non_printable(char *str);
+
+int main()
+{
+	char *str = malloc(256);
+
+	printf("\n");
+	for(int i = 0; i < 256; i++)
+	{
+		str[i] = (i + 1) % 256;
+	}	
+	ft_putstr_non_printable(str);
+	printf("\n");
+	
+	free(str);
+	return (0);
+}
