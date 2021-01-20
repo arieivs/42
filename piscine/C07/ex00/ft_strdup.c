d92d8c66142a85868274eebbec9d3e92fd942bfd
Sara Vieira <s.cvieira@sapo.pt>
2021-01-20 10:03:21 +0000

    strdup done

---
+++ piscine/C07/ex00/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/20 09:45:54 by svieira           #+#    #+#             */
+/*   Updated: 2021/01/20 10:00:39 by svieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		size;
+	char	*dest;
+	int		i;
+
+	size = 0;
+	while (src[size])
+		size++;
+	dest = (char *)malloc(sizeof(src) * (size + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[size] = 0;
+	return (dest);
+}
