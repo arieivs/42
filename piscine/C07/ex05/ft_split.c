ad1a6dc120380bb502c34f6e4401d2a82e3f8aa8
Sara Vieira <s.cvieira@sapo.pt>
2021-01-22 11:13:22 +0000

    protected mallocs in ft_split

--- piscine/C07/ex05/ft_split.c
+++ piscine/C07/ex05/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 09:44:26 by svieira           #+#    #+#             */
-/*   Updated: 2021/01/22 10:29:52 by svieira          ###   ########.fr       */
+/*   Updated: 2021/01/22 11:12:38 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,18 +40,14 @@ int		words_count(char *str, char *charset)
 	return (words);
 }
 
-char	**ft_split(char *str, char *charset)
+char	**ft_fill_arr(char *str, char *charset, int words, char **strs)
 {
-	int		i;
-	int		word;
-	int		words;
-	int		letters;
-	char	**strs;
+	int i;
+	int word;
+	int letters;
 
 	i = 0;
 	word = 0;
-	words = words_count(str, charset);
-	strs = (char **)malloc(sizeof(char*) * (words + 1));
 	while (word < words)
 	{
 		while (str[i] && is_charset(str[i], charset))
@@ -60,11 +56,26 @@ char	**ft_split(char *str, char *charset)
 		while (str[i + letters] && !is_charset(str[i + letters], charset))
 			letters++;
 		strs[word] = (char *)malloc(sizeof(char) * (letters + 1));
+		if (!strs[word])
+			return (NULL);
 		letters = 0;
 		while (str[i] && !is_charset(str[i], charset))
 			strs[word][letters++] = str[i++];
 		strs[word++][letters] = 0;
 	}
 	strs[word] = 0;
+	return (strs);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	int		words;
+	char	**strs;
+
+	words = words_count(str, charset);
+	strs = (char **)malloc(sizeof(char*) * (words + 1));
+	if (!strs)
+		return (NULL);
+	ft_fill_arr(str, charset, words, strs);
 	return (strs);
 }
