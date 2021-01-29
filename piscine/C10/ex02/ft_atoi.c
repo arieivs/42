3960a441801df2acfdb2cd6c16813685d0a020e7
Sara Vieira <s.cvieira@sapo.pt>
2021-01-29 14:53:08 +0000

    copied ft_atoi and modified it: it only receives clean input and the signal is irrelevant

---
+++ piscine/C10/ex02/ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/09 18:26:33 by svieira           #+#    #+#             */
+/*   Updated: 2021/01/29 14:48:15 by svieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_tail.h"
+
+int	is_signal(char c)
+{
+	return (c == '+' || c == '-');
+}
+
+int	is_number(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	ft_atoi(char *str)
+{
+	int i;
+	int nb;
+
+	i = 0;
+	nb = 0;
+	if (str[i] && is_signal(str[i]))
+		i++;
+	while (str[i] && is_number(str[i]))
+	{
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
+	return (nb);
+}
