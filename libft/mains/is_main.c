7a1f8cb1617b28e40d495bef9393fcb521b01faa
Sara Vieira <s.cvieira@sapo.pt>
2021-02-08 22:28:42 +0000

    isdigit and updated is_main

--- libft/mains/is_main.c
+++ libft/mains/is_main.c
@@ -1,6 +1,9 @@
 #include <ctype.h>
 #include <stdio.h>
 
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+
 int	main(int ac, char **av)
 {
 	int i;
@@ -10,16 +13,20 @@ int	main(int ac, char **av)
 	while (i < ac)
 	{
 		c = av[i][0];
-		printf("isalpha %c is ", av[i][0]);
+		printf("original: %c is ", av[i][0]);
 		if (isalpha(c))
 			printf("ALPHA\n");
+		else if (isdigit(c))
+			printf("DIGIT\n");
 		else
-			printf("NOT ALPHA\n");
-		printf("ft_isalpha %c is ", av[i][0]);
-		if (isalpha(c))
+			printf("a thing\n");
+		printf("my ft: %c is ", av[i][0]);
+		if (ft_isalpha(c))
 			printf("ALPHA\n\n");
+		else if (ft_isdigit(c))
+			printf("DIGIT\n\n");
 		else
-			printf("NOT ALPHA\n\n");
+			printf("a thing\n\n");
 		i++;
 	}
 	return (0);
