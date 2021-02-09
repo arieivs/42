7864c01303142fbfbc785dc64e4df0b317f8c3ad
Sara Vieira <s.cvieira@sapo.pt>
2021-02-09 17:00:13 +0000

    bzero and updated mem_main

--- libft/mains/mem_main.c
+++ libft/mains/mem_main.c
@@ -3,17 +3,30 @@
 #include <stdio.h>
 
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
 
 int main(void)
 {
-	size_t	len = 5;
-	int		c = 65;
 	char	my_b[6] = "";
 	char	or_b[6] = "";
+	char	my_a[] = "hello there!!";
+	char	or_a[] = "hello there!!";
 
-	ft_memset((void *)my_b, c, len);
-	memset((void *)or_b, c, len);
+	printf("MEMSET\n");
+	ft_memset((void *)my_b, 65, 5);
+	memset((void *)or_b, 65, 5);
+	printf("1 - mine: %s\n", my_b);
+	printf("    orgn: %s\n", or_b);
+
+	ft_memset((void *)my_a + 3, 64, 5);
+	memset((void *)or_a + 3, 64, 5);
+	printf("2 - mine: %s\n", my_a);
+	printf("    orgn: %s\n\n", or_a);
+
+	printf("BZERO\n");
+	ft_bzero((void *)my_b, 6);
+	bzero((void *)or_b, 6);
 	printf("mine: %s\n", my_b);
-	printf("orgn: %s\n", or_b);
+	printf("orgn: %s\n\n", or_b);
 	return (0);
 }
