7517e78344d0a1a2505cb431aac3b057b50e0e12
Sara Vieira <s.cvieira@sapo.pt>
2021-02-11 16:45:09 +0000

    strlcpy and updated str_main

---
+++ libft/mains/str_main.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+
+int main(void)
+{
+	char	s1[] = "string";
+	char	my_s2[10] = "";
+	char	or_s2[10] = "";
+	size_t	my_len;
+	size_t	or_len;
+
+	printf("\nSTRLEN\n");
+	my_len = ft_strlen(s1);
+	or_len = strlen(s1);
+	printf("original length: %zu, my length: %zu\n", or_len, my_len);
+
+	printf("\nSTRLCPY\n");
+	my_len = ft_strlcpy(my_s2, s1, 0);
+	or_len = strlcpy(or_s2, s1, 0);
+	printf("1 - orgn: length %zu   dst %s\n", or_len, or_s2);
+	printf("    mine: length %zu   dst %s\n", my_len, my_s2);	
+	my_len = ft_strlcpy(my_s2, s1, 4);
+	or_len = strlcpy(or_s2, s1, 4);
+	printf("2 - orgn: length %zu   dst %s\n", or_len, or_s2);
+	printf("    mine: length %zu   dst %s\n", my_len, my_s2);
+	my_len = ft_strlcpy(my_s2, s1, 8);
+	or_len = strlcpy(or_s2, s1, 8);
+	printf("3 - orgn: length %zu   dst %s\n", or_len, or_s2);
+	printf("    mine: length %zu   dst %s\n", my_len, my_s2);
+
+	return (0);
+}
