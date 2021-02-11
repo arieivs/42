42fe050dc84c3681cc93feccbf9b46966ecc7192
Sara Vieira <s.cvieira@sapo.pt>
2021-02-11 17:51:22 +0000

    strlcat and str_main updated

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/11 17:08:48 by svieira           #+#    #+#             */
+/*   Updated: 2021/02/11 17:20:40 by svieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dst[i])
+		i++;
+	while (src[j] && i + j < dstsize - 1 && dstsize != 0)
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (dstsize != 0 || dstsize < i)
+		dst[i + j] = 0;
+	while (src[j])
+		j++;
+	if (dstsize < i)
+		return (dstsize + j);
+	return (i + j);
+}
--- libft/mains/str_main.c
+++ libft/mains/str_main.c
@@ -3,12 +3,13 @@
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, char *src, size_t dstsize);
 
 int main(void)
 {
 	char	s1[] = "string";
-	char	my_s2[10] = "";
-	char	or_s2[10] = "";
+	char	my_s2[20] = "";
+	char	or_s2[20] = "";
 	size_t	my_len;
 	size_t	or_len;
 
@@ -29,6 +30,24 @@ int main(void)
 	my_len = ft_strlcpy(my_s2, s1, 8);
 	or_len = strlcpy(or_s2, s1, 8);
 	printf("3 - orgn: length %zu   dst %s\n", or_len, or_s2);
+	printf("    mine: length %zu   dst %s\n", my_len, my_s2);
+
+	printf("\nSTRLCAT\n");
+	my_len = ft_strlcat(my_s2, s1, 0);
+	or_len = strlcat(or_s2, s1, 0);
+	printf("1 - orgn: length %zu    dst %s\n", or_len, or_s2);
+	printf("    mine: length %zu    dst %s\n", my_len, my_s2);	
+	my_len = ft_strlcat(my_s2, s1, 4);
+	or_len = strlcat(or_s2, s1, 4);
+	printf("2 - orgn: length %zu   dst %s\n", or_len, or_s2);
+	printf("    mine: length %zu   dst %s\n", my_len, my_s2);
+	my_len = ft_strlcat(my_s2, s1, 10);
+	or_len = strlcat(or_s2, s1, 10);
+	printf("3 - orgn: length %zu   dst %s\n", or_len, or_s2);
+	printf("    mine: length %zu   dst %s\n", my_len, my_s2);
+	my_len = ft_strlcat(my_s2, s1, 18);
+	or_len = strlcat(or_s2, s1, 18);
+	printf("4 - orgn: length %zu   dst %s\n", or_len, or_s2);
 	printf("    mine: length %zu   dst %s\n", my_len, my_s2);
 
 	return (0);
