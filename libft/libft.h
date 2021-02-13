9c08f25fc821bf40d5e8b65d439f106f9c18c2a2
Sara Vieira <s.cvieira@sapo.pt>
2021-02-13 12:51:08 +0000

    added missing prototypes to libft.h, norminette still thinks it wont compile

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 19:28:24 by svieira           #+#    #+#             */
-/*   Updated: 2021/02/12 12:26:44 by svieira          ###   ########.fr       */
+/*   Updated: 2021/02/13 12:46:32 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,13 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
