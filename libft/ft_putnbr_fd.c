a737769915858172c6ebcfaa85ba65b2432e1120
Sara Vieira <s.cvieira@sapo.pt>
2021-02-15 19:38:03 +0000

    punbr and updated put_main to write to a text file, added the test text file to gitignore

---
+++ libft/ft_putnbr_fd.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/08 21:35:15 by svieira           #+#    #+#             */
+/*   Updated: 2021/02/15 19:10:07 by svieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	ft_putnegnbr_fd(int n, int fd)
+{
+	if (n <= -10)
+		ft_putnegnbr_fd(n / 10, fd);
+	ft_putchar_fd(-(n % 10) + '0', fd);
+}
+
+void		ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnegnbr_fd(n, fd);
+	}
+	else
+		ft_putnegnbr_fd(-n, fd);
+}
--- libft/mains/put_main.c
+++ libft/mains/put_main.c
@@ -1,16 +1,44 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "../libft.h"
+#include <stdio.h>
 
-int	main(void)
+int	main(int ac, char **av)
 {
+	int		fd;
 	char	c = 'c';
 	char	s[] = "To be or not to be";
+	int		x = 453910;
+	int		y = -2147483648;
 
-	write(1, "\nPUTS\n", 6);	
-	ft_putchar_fd(c, 1);
-	write(1, "   ", 3);
-	ft_putstr_fd(s, 1);
-	write(1, "   ", 3);
-	ft_putendl_fd(s, 1);
-
+	if (ac != 2)
+	{
+		printf("Please give one filename as argument\n");
+		return (0);
+	}
+	fd = open(av[1], O_WRONLY);
+	if (fd == -1)
+	{
+		write(2, "Cannot open file.\n", 18);
+		return (-1);
+	}
+	write(fd, "PUTS\n", 6);	
+	ft_putchar_fd(c, fd);
+	write(fd, "   ", 3);
+	ft_putstr_fd(s, fd);
+	write(fd, "   ", 3);
+	ft_putendl_fd(s, fd);
+	ft_putnbr_fd(x, fd);
+	write(fd, "   ", 3);
+	ft_putnbr_fd(y, fd);
+	write(fd, "\n", 1);
+	printf("cat file_name should print the following:\n");
+	printf("PUTS\n%c   %s   %s\n%d   %d\n", c, s, s, x, y);
+	if (close(fd) == -1)
+	{
+		write(2, "Cannot close file.\n", 19);
+		return (-1);
+	}
 	return (0);
 }
