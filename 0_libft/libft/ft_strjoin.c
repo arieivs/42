5dc830d98e5dab0e7cf51e7c31f457df55627e89
Sara Vieira <s.cvieira@sapo.pt>
2021-02-19 12:13:22 +0000

    fix in malloc from strjoin, sizeof(s) is clearly not a good thing, liars

--- 0_libft/libft/ft_strjoin.c
+++ 0_libft/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 11:52:49 by svieira           #+#    #+#             */
-/*   Updated: 2021/02/16 12:18:36 by svieira          ###   ########.fr       */
+/*   Updated: 2021/02/19 12:05:24 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strjoin(const char *s1, const char *s2)
 
 	size1 = ft_strlen(s1);
 	size2 = ft_strlen(s2);
-	s3 = (char *)malloc(sizeof(s3) * (size1 + size2 + 1));
+	s3 = (char *)malloc(sizeof(char) * (size1 + size2 + 1));
 	if (!s3)
 		return (NULL);
 	i = 0;
