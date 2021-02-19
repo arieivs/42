479d44aabd978b94a9b930e5d8fd83b37e68025b
Sara Vieira <s.cvieira@sapo.pt>
2021-02-19 16:09:40 +0000

    lstadd_front, lstsize and updated lst_main

--- 0_libft/mains/lst_main.c
+++ 0_libft/mains/lst_main.c
@@ -3,11 +3,23 @@
 
 int	main(void)
 {
-	int		data = 10;
-	t_list	*list;
+	int		x1 = 5;
+	int		x2 = 10;
+	t_list	*list1;
+	t_list	*list2;
 
-	printf("\nLSTNEW\n");
-	list = ft_lstnew((void *)&data);
-	printf("new list content: %d\n", *(int *)list->content);
+	printf("\nLST NEW\n");
+	list1 = ft_lstnew((void *)&x1);
+	printf("new list content: %d\n", *(int *)list1->content);
+
+	printf("\nLST ADD FRONT\n");
+	list2 = ft_lstnew((void *)&x2);
+	ft_lstadd_front(&list1, list2);
+	printf("lst->content - expect: 10, got %d\n", *(int *)list1->content);
+	printf("lst->next->content - expect: 5, got %d\n", *(int *)list1->next->content);
+
+	printf("\nLST SIZE\n");
+	printf("expect: 2, got %d\n", ft_lstsize(list1));
+
 	return (0);
 }
