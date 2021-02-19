37590fcb4f1154db583e96607c959e84bc393905
Sara Vieira <s.cvieira@sapo.pt>
2021-02-19 16:37:08 +0000

    lst add back and updated lst_main

---
+++ 0_libft/libft/ft_lstadd_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/19 16:22:33 by svieira           #+#    #+#             */
+/*   Updated: 2021/02/19 16:30:34 by svieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*curr;
+
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	curr = *lst;
+	while (curr->next)
+		curr = curr->next;
+	curr->next = new;
+}
--- 0_libft/mains/lst_main.c
+++ 0_libft/mains/lst_main.c
@@ -5,8 +5,10 @@ int	main(void)
 {
 	int		x1 = 5;
 	int		x2 = 10;
+	int		x3 = 15;
 	t_list	*list1;
 	t_list	*list2;
+	t_list	*listend;
 
 	printf("\nLST NEW\n");
 	list1 = ft_lstnew((void *)&x1);
@@ -18,11 +20,18 @@ int	main(void)
 	printf("lst->content - expect: 10, got %d\n", *(int *)list1->content);
 	printf("lst->next->content - expect: 5, got %d\n", *(int *)list1->next->content);
 
+	printf("\nLST ADD BACK\n");
+	listend = ft_lstnew((void *)&x3);
+	ft_lstadd_back(&list1, listend);
+	printf("lst->cntt - expect: 10, got %d\n", *(int *)list1->content);
+	printf("lst->next->cntt - expect: 5, got %d\n", *(int *)list1->next->content);
+	printf("lst->next->next->cntt - expect: 15, got %d\n", *(int *)list1->next->next->content);
+
 	printf("\nLST SIZE\n");
-	printf("expect: 2, got %d\n", ft_lstsize(list1));
+	printf("expect: 3, got %d\n", ft_lstsize(list1));
 
 	printf("\nLST LAST\n");
-	printf("expect: 5, got %d\n", *(int *)(ft_lstlast(list1)->content));
+	printf("expect: 15, got %d\n", *(int *)(ft_lstlast(list1)->content));
 
 	return (0);
 }
