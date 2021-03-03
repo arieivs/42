e777576bc094cdc56c61ecd27eae50c5e8e41438
Sara Vieira <svieira@c1r6s6.42lisboa.com>
2021-03-03 18:13:06 +0000

    trying

--- 1_get_next_line/get_next_line.c
+++ 1_get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 11:13:52 by svieira           #+#    #+#             */
-/*   Updated: 2021/02/24 20:11:58 by svieira          ###   ########.fr       */
+/*   Updated: 2021/03/03 18:12:49 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,26 @@
 
 int	get_next_line(int fd, char **line)
 {
-	int		read_size;
+	static char	*curr_line = NULL;
+	char		*next_line;
+	int			read_size;
+	char		buff[BUFFER_SIZE + 1];
 
-	read_size = read(fd, *line, BUFFER_SIZE);
-	*line[read_size] = 0;
-	// assim recebo a mais:
-	if (read_size <= 0)
-		return (read_size);
-	// assim recebo a menos:
-	/*if (BUFFER_SIZE > read_size)
-		return (0);*/
-	return (1);
+	if (curr_line = NULL)
+		curr_line = (char *)malloc(sizeof(char));
+	next_line = ft_strchr(curr_line, '\n');
+	if (next_line)
+		// *line = curr_line until \n
+		// currline = nex_tline
+		// return (1) ?
+	// *line = curr_line
+	while ((read_size = read(fd, buff, BUFFER_SIZE)) > 0 && !ft_strchr(buff, '\n'))
+	{
+		buff[read_size] = 0;
+		// join buff to end of *line
+	}
+	read_size = read(fd, buff, BUFFER_SIZE);
+	// join buff until \n to end of *line
+	// curr_line = resto do buff (dps do \n)
+	//return 0 or 1 accordinggly
 }
