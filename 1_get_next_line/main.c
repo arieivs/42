6fefa63d6fc8d8b91428afd511e07a6db5ec7ad5
Sara Vieira <s.cvieira@sapo.pt>
2021-03-04 22:53:56 +0000

    changed main so that it can read from stdin

--- 1_get_next_line/main.c
+++ 1_get_next_line/main.c
@@ -4,18 +4,37 @@
 
 int	get_next_line(int fd, char **line);
 
-int	main(void)
+int	main(int ac, char **av)
 {
 	int		fd;
 	char	*buff;
 	int		ret;
 	int		line;
 
-	fd = open("text_file", O_RDWR | O_CREAT);
+	if (ac > 2)
+	{
+		printf("Please give me one file at a time\n");
+		return (0);
+	}
 	buff = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	line = 1;
-	while ((ret = get_next_line(fd, &buff)) > 0)
-		printf("%d - %s - returned %d\n", line++, buff, ret);
+	if (ac == 1)
+	{
+		fd = 1;
+		while ((ret = get_next_line(fd, &buff)) > 0)
+			printf("%d-%d: %s\n", ret, line++, buff);
+	}
+	else
+	{
+		fd = open(av[1], O_RDWR | O_CREAT);
+		while ((ret = get_next_line(fd, &buff)) > 0)
+			printf("%d-%d: %s\n", ret, line++, buff);
+		printf("%d-%d: %s\n", ret, line++, buff);
+	}
+	if (ret == 0)
+		printf("Reached EOF\n");
+	if (ret == -1)
+		printf("An Error occurred\n");
 	close(fd);
 	free(buff);
 	return (0);
