760d63288ebd987168569b4d6d6abb0e48150113
Sara Vieira <svieira@c1r6s6.42lisboa.com>
2021-03-24 15:46:26 +0000

    understanding variadic functions

--- 1_printf/play.c
+++ 1_printf/play.c
@@ -1,5 +1,51 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include <stdio.h>
+
+void	ft_putstr(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	var_ft(char *str, ...)
+{
+	int		i = 0;
+	va_list	pa;
+	char	c;
+	char	*s;
+
+	va_start(pa, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			if (str[i] == 'c')
+			{
+				c = va_arg(pa, int);
+				write(1, &c, 1);
+			}
+			else if (str[i] == 's')
+			{
+				s = va_arg(pa, char *);
+				ft_putstr(s);
+			}
+		}
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+	va_end(pa);
+}
 
 int	main(void)
 {
-
+	var_ft("hel%co %s%cess\n", 'l', "dark", 'n');
+	return (0);
+}
