061568c34983fcd93d843aa7e223d29f8dac2cc6
Sara Vieira <s.cvieira@sapo.pt>
2021-03-29 19:24:43 +0100

    includes .h file

---
+++ 1_printf/ft_printf.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/29 19:16:40 by svieira           #+#    #+#             */
+/*   Updated: 2021/03/29 19:22:05 by svieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct	s_fmt
+{
+	char	plus;
+	int		left_align;
+	char	fill;
+	int		width;
+	int		point;
+	int		precision;
+}				t_fmt;
+
+t_fmt			*init_fmt(void);
+int				parse(char *str, t_fmt *fmt, va_list ap);
+int				print_fmt(t_fmt *fmt, va_list ap);
+
+#endif
