990204b7fce8f8fb6ef33447bb084e3dec20734f
Sara Vieira <s.cvieira@sapo.pt>
2021-04-05 10:51:43 +0100

    making sure it still works

--- 1_printf/main.c
+++ 1_printf/main.c
@@ -4,45 +4,45 @@
 int	main(void)
 {
 	int	a = 2;
-	/*int	orig;
+	int	orig;
 	int	mine;
 
-	printf("\nd\n");
+	printf("\n** d **\n");
 	orig = printf("you%.*dtoo\n", 0, 0);
 	mine = ft_printf("you%.*dtoo\n", 0, 0);
-	printf("\norig: %d mine: %d\n", orig, mine);
+	printf("orig: %d mine: %d\n\n", orig, mine);
 	orig = printf("hey %0+2.2d yo %*.*d\n", 42, 10, 6, 5982);
 	mine = ft_printf("hey %0+2.2d yo %*.*d\n", 42, 10, 6, 5982);
-	printf("\norig: %d mine: %d\n", orig, mine);
+	printf("orig: %d mine: %d\n\n", orig, mine);
 	
-	printf("\nx and X\n");
+	printf("\n** x and X **\n");
 	orig = printf("0*%-*x*0 0*%0*X*0 \n", 21, 1021, 21, 1011);
 	mine = ft_printf("0*%-*x*0 0*%0*X*0 \n", 21, 1021, 21, 1011);
-	printf("\norig: %d mine: %d\n", orig, mine);
+	printf("orig: %d mine: %d\n\n", orig, mine);
 
-	printf("\nc\n");
+	printf("\n** c **\n");
 	orig = printf("hey%5cyou\n", 345);
 	mine = ft_printf("hey%5cyou\n", 345);
-	printf("\norig: %d mine: %d\n", orig, mine);
+	printf("orig: %d mine: %d\n\n", orig, mine);
 	orig = printf("hey%5cyou\n", 0);
 	mine = ft_printf("hey%5cyou\n", 0);
-	printf("\norig: %d mine: %d\n", orig, mine);
+	printf("orig: %d mine: %d\n\n", orig, mine);
 
-	printf("\ns\n");
+	printf("\n** s **\n");
 	orig = printf("<%10.2s>\n", "hello");
 	mine = ft_printf("<%10.2s>\n", "hello");
-	printf("\norig: %d mine: %d\n", orig, mine);
+	printf("orig: %d mine: %d\n\n", orig, mine);
 	printf("34<%-10s>\n", NULL);
 	ft_printf("34<%-10s>\n", NULL);
-	printf("\norig: %d mine: %d\n", orig, mine);
+	printf("orig: %d mine: %d\n\n", orig, mine);
 	printf("35<%-9.1s>\n", NULL);
 	ft_printf("35<%-9.1s>\n", NULL);
-	printf("\norig: %d mine: %d\n", orig, mine);
+	printf("orig: %d mine: %d\n\n", orig, mine);
 	printf("36<%.*s>\n", -2, NULL);
 	ft_printf("36<%.*s>\n", -2, NULL);
-	printf("\norig: %d mine: %d\n", orig, mine);*/
+	printf("orig: %d mine: %d\n\n", orig, mine);
 
-	printf("\np\n");
+	printf("\n** p **\n");
 	printf("testing %p\n", &a);
 	return (0);
 }
