c21cd6d354eadd36fe3c1e5f955c16d258aceaf5
Sara Vieira <s.cvieira@sapo.pt>
2021-05-17 17:10:52 +0100

    pseudo coding

---
+++ 2_push_swap/sort.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/17 16:40:32 by svieira           #+#    #+#             */
+/*   Updated: 2021/05/17 16:56:14 by svieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* BUBBLE SORT
+keep on raing
+when one is bigger than the next, sa, then ra
+need to know how big the stack is
+keep doing it until it's sorted
+alternative: pb instead of ra
+*/
+
+/* SELECTION SORT
+go though the list and find the smallest
+pb all of it including the smallest
+rb
+pa everything back except for the smallest
+do it all again until it's sorted
+*/
+
+/*Both strategies seem largely inefficient...*/
