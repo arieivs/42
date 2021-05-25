75448d8bc13171d2821d90fed81a218bb4ffb96c
Sara Vieira <s.cvieira@sapo.pt>
2021-05-25 14:47:14 +0100

    pseudo coding a bit more

--- 2_push_swap/sort.c
+++ 2_push_swap/sort.c
@@ -6,7 +6,7 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 16:40:32 by svieira           #+#    #+#             */
-/*   Updated: 2021/05/17 16:56:14 by svieira          ###   ########.fr       */
+/*   Updated: 2021/05/25 13:13:21 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,12 @@ do it all again until it's sorted
 */
 
 /*Both strategies seem largely inefficient...*/
+
+/* QUICK SORT
+ * Iterate over the stack A (ra's)
+ * if elememt is smaller than average, push to b (pb)
+ * right now we have: upper half in A, lower half in B
+ * Iterate over the stack B (rb's)
+ * if element is bigger than average in B, push to A (pa)
+ * if element is the minimum in the whole B stack, push to bottom of A (pa + ra)
+ * to think: how do I keep track of the minimum? I could of course, order an array with the same content of the stack to compare... but... I mean...
