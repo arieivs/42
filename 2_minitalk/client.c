6cce093b5d182f03b2b9b4d5896ddb92ee41cf17
Sara Vieira <s.cvieira@sapo.pt>
2021-10-07 23:16:48 +0200

    client is talking to server, server is listening

--- 2_minitalk/client.c
+++ 2_minitalk/client.c
@@ -1,4 +1,15 @@
-int	main(void)
+#include "minitalk.h"
+
+int	main(int ac, char **av)
 {
+	pid_t	server_pid;
+
+	if (ac != 3)
+	{
+		write(1, "Please provide the server's PID and a message.\n", 47);
+		return (0);
+	}
+	server_pid = ft_atoi(av[1]);
+	kill(server_pid, SIGUSR1);
 	return (0);
 }
--- 2_minitalk/server.c
+++ 2_minitalk/server.c
@@ -1,14 +1,28 @@
 #include "minitalk.h"
 
+void	server_sig_handler(int sig_num)
+{
+	(void)sig_num;
+	struct sigaction	sa;
+	
+	sa.sa_handler = &server_sig_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	write(1, "Signal received!\n", 17);
+}
+
 int	main(void)
 {
-	pid_t	pid;
+	pid_t				pid;
+	struct sigaction	sa;
 
+	sa.sa_handler = &server_sig_handler;
+	sigaction(SIGUSR1, &sa, NULL);
 	pid = getpid();
 	write(1, "PID: ", 5);
 	ft_putnbr_fd(pid, 1);
 	write(1, "\n", 1);
-	pause();
+	while (1)
+		pause();
 	return(0);
 }
 	
