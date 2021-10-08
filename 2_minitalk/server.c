5b5440a1eae62a4456ff3b893682ca2b4ab8634d
Sara Vieira <s.cvieira@sapo.pt>
2021-10-08 17:05:24 +0200

    receiving chars, but not full strings

--- 2_minitalk/server.c
+++ 2_minitalk/server.c
@@ -1,33 +1,43 @@
 #include "minitalk.h"
 
-void	sigusr1_handler(int sig_num)
+void	sigusr_handler(int sig_num)
 {
-	(void)sig_num;
+	static int	nr_bits = 0;
+	static char	c = 0;
 
-	write(1, "1", 1);
-}
-
-void	sigusr2_handler(int sig_num)
-{
-	(void)sig_num;
-
-	write(1, "0", 1);
+	if (sig_num == SIGUSR1)
+	{
+		write(1, "1", 1);
+		c = c | 1;
+	}
+	else
+	{
+		write(1, "0", 1);
+		c = c | 0;
+	}
+	nr_bits++;
+	if (nr_bits >= 8)
+	{
+		write(1, &c, 1);
+		write(1, "\n", 1);
+		nr_bits = 0;
+		c = 0;
+	}
+	else
+		c = c << 1;	
 }
 
 int	main(void)
 {
 	pid_t				pid;
-	struct sigaction	sa1;
-	struct sigaction	sa2;
+	struct sigaction	sa;
 
-	sa1.sa_handler = &sigusr1_handler;
-	sigemptyset(&(sa1.sa_mask));
-	sigaddset(&(sa1.sa_mask), SIGUSR1);
-	sigaction(SIGUSR1, &sa1, NULL);
-	sa2.sa_handler = &sigusr2_handler;
-	sigemptyset(&(sa2.sa_mask));
-	sigaddset(&(sa2.sa_mask), SIGUSR2);
-	sigaction(SIGUSR2, &sa2, NULL);
+	sa.sa_handler = &sigusr_handler;
+	sigemptyset(&(sa.sa_mask));
+	sigaddset(&(sa.sa_mask), SIGUSR1);
+	sigaddset(&(sa.sa_mask), SIGUSR2);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	pid = getpid();
 	write(1, "PID: ", 5);
 	ft_putnbr_fd(pid, 1);
