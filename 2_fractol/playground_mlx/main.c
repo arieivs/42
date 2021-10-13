97ae0ed486e97c7471f1d6b9ca2891a26e407fa6
Sara Vieira <s.cvieira@sapo.pt>
2021-10-13 19:25:39 +0200

    drawing circles

--- 2_fractol/playground_mlx/main.c
+++ 2_fractol/playground_mlx/main.c
@@ -10,11 +10,6 @@ typedef struct s_img_info {
 	int		endian;
 }				t_img_info;
 
-typedef struct s_coords {
-	int	x;
-	int	y;
-}				t_coords;
-
 void	my_pixel_put(t_img_info *img, int x, int y, int color)
 {
 	char	*pixel;
@@ -69,6 +64,31 @@ void	draw_square(t_img_info *img, int xi, int yi, int size, int color)
 	}
 }	
 
+void	draw_circle(t_img_info *img, int x0, int y0, int r, int color)
+{
+	int	x;
+	int	y;
+
+	x = x0 - r;
+	while (x <= x0 + r)
+	{
+		y = y0 + sqrt(r * r - (x - x0) * (x - x0));
+		my_pixel_put(img, x, y, color); 
+		y = y0 - sqrt(r * r - (x - x0) * (x - x0));
+		my_pixel_put(img, x, y, color); 
+		x++;
+	}
+	y = y0 - r;
+	while (y <= y0 + r)
+	{
+		x = x0 + sqrt(r * r - (y - y0) * (y - y0));
+		my_pixel_put(img, x, y, color); 
+		x = x0 - sqrt(r * r - (y - y0) * (y - y0));
+		my_pixel_put(img, x, y, color); 
+		y++;
+	}
+}
+
 int	main(void)
 {
 	void		*mlx;
@@ -80,10 +100,11 @@ int	main(void)
 	img.img = mlx_new_image(mlx, 600, 300);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
 		&img.line_length, &img.endian);
-	draw_line(&img, 5, 5, 5, 5, 0x00FF0000);
-	draw_line(&img, 6, 120, 6, 6, 0x0000FF00);
-	draw_line(&img, 20, 120, 120, 20, 0x00FFFFFF);
-	draw_square(&img, 100, 50, 100, 0x000000FF);
+	draw_line(&img, 5, 5, 5, 5, 0x00FFFFFF); // white dot
+	draw_line(&img, 6, 120, 6, 6, 0x0002C0A0); // EYEN green line
+	draw_line(&img, 20, 120, 120, 20, 0x00FBF3E4); // EYEN bege line
+	draw_square(&img, 100, 50, 100, 0x000000BB); // EYEN blue square
+	draw_circle(&img, 500, 100, 50, 0x00FF9900); // EYEN orange circle
 	mlx_put_image_to_window(mlx, mlx_window, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
