182fd22262157b66eeff8872780bca3cd1ba8ef5
Sara Vieira <s.cvieira@sapo.pt>
2021-11-13 12:30:16 +0000

    clicking on julia set will change c 🤩

--- 2_fractol/hooks.c
+++ 2_fractol/hooks.c
@@ -6,7 +6,7 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 13:02:02 by svieira           #+#    #+#             */
-/*   Updated: 2021/11/13 12:00:20 by svieira          ###   ########.fr       */
+/*   Updated: 2021/11/13 12:29:11 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,20 @@ void	zoom(t_vars *vars, int zoom_in, int x, int y)
 	put_fractal_to_window(vars);
 }
 
+void	change_c(t_vars *vars, int x, int y)
+{
+	vars->c_re_julia = vars->min_re + vars->step * (double)x;
+	vars->c_im_julia = vars->max_im - vars->step * (double)y;
+	put_fractal_to_window(vars);
+	printf("new c: %.3f %+.3f i\n", vars->c_re_julia, vars->c_im_julia);
+}
+
 int	mouse_hook(int button, int x, int y, t_vars *vars)
 {
 	if (button == 4 || button == 5)
 		zoom(vars, button == 4, x, y);
+	if (button == 1 && vars->fractal == 'j')
+		change_c(vars, x, y);
 	return (0);
 }
 
