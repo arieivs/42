d30afbcea6073d0d2e49d86ecf5a9fac7fa78132
Sara Vieira <s.cvieira@sapo.pt>
2021-11-14 01:07:28 +0000

    small changes to change the orientation

--- 2_fractol/burnships.c
+++ 2_fractol/burnships.c
@@ -6,7 +6,7 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 17:24:54 by svieira           #+#    #+#             */
-/*   Updated: 2021/11/13 13:26:02 by svieira          ###   ########.fr       */
+/*   Updated: 2021/11/14 01:03:09 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ static int	iterate_burnships(double c_re, double c_im)
 	z_im = 0;
 	while (++n < MAX_ITERATIONS)
 	{
-		z_re_tmp = z_re * z_re - (z_im * z_im) - c_re;
+		z_re_tmp = z_re * z_re - (z_im * z_im) + c_re;
 		mod = z_re * z_im;
 		if (mod < 0)
 			mod *= -1;
-		z_im = 2 * mod - c_im;
+		z_im = 2 * mod + c_im;
 		z_re = z_re_tmp;
 		if ((z_re * z_re + z_im * z_im) > 4)
 			return (n);
@@ -48,7 +48,7 @@ void	burnships(t_vars *vars)
 	y = 0;
 	while (y < HEIGHT)
 	{
-		c_im = vars->max_im - vars->step * y;
+		c_im = - vars->max_im + vars->step * y;
 		x = 0;
 		while (x < WIDTH)
 		{
