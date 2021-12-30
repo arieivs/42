24b617dce6979367d3ceaede487db3874e15008a
Sara Vieira <s.cvieira@sapo.pt>
2021-12-30 23:46:49 +0000

    backbone of thread creation

--- 3_philosophers/philosophers.h
+++ 3_philosophers/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 18:58:14 by svieira           #+#    #+#             */
-/*   Updated: 2021/12/30 19:02:04 by svieira          ###   ########.fr       */
+/*   Updated: 2021/12/30 21:10:55 by svieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@
 # include <pthread.h>
 # include <stdio.h>
 
-typedef struct s_philosopher {
+typedef struct s_philo_data
+{
 	int				id;
 	pthread_mutex_t	*right_fork;
 	pthread_mutex_t	*left_fork;
-}				t_philosopher;
+}				t_philo_data;
 
 #endif
--- 3_philosophers/trial.c
+++ 3_philosophers/trial.c
@@ -1,26 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   trial.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/30 20:40:30 by svieira           #+#    #+#             */
+/*   Updated: 2021/12/30 23:46:32 by svieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philosophers.h"
+
+t_philo_data	philo_data_init(int id, pthread_mutex_t *right_fork,
+				pthread_mutex_t *left_fork)
+{
+	t_philo_data	philo_data;
+
+	philo_data.id = id;
+	philo_data.right_fork = right_fork;
+	philo_data.left_fork = left_fork;
+	return (philo_data);
+}
+
+void	*live(void *philo_data)
+{
+	// live
+	// call eat, sleep, think
+	printf("Philosopher %d\n", ((t_philo_data *)philo_data)->id);
+	return (NULL);
+}
 
 int	main(void)
 {
-	int	n = 3;
-	pthread_t		*philosophers;
+	int	n = 3; // which will come from argv later
 	pthread_mutex_t	*forks;
+	t_philo_data	*philos_data;
+	pthread_t		*philos_threads;
 	int				i;
 
+	forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * n);
 	i = 0;
 	while (i < n)
-	{
 		pthread_mutex_init(&forks[i++], NULL);
-		printf("fork %d created\n", i);
-	}
+	philos_data = (t_philo_data *)malloc(sizeof(t_philo_data) * n);
+	philos_threads = (pthread_t *)malloc(sizeof(pthread_t) * n);
 	i = 0;
 	while (i < n)
 	{
 		if (i == 0)
-			data_philosopher_init(i + 1, &forks[n - 1], &forks[0])
+			philos_data[i] = philo_data_init(i + 1, &forks[n - 1], &forks[0]);
 		else
-			data_philosopher_init(i + 1, &forks[i - 1], &forks[i])
-		// create thread with struct
-		i++
+			philos_data[i] = philo_data_init(i + 1, &forks[i - 1], &forks[i]);
+		if (pthread_create(&philos_threads[i], NULL, &live,
+			(void *)&philos_data[i]))
+			return (1);
+		i++;
 	}
-
+	i = 0;
+	while (i < n)
+	{
+		if (pthread_join(philos_threads[i], NULL))
+			return (1);
+		i++;
+	}
+	free(philos_threads);
+	free(philos_data);
+	i = 0;
+	while (i < n)
+		pthread_mutex_destroy(&forks[i++]);
+	free(forks);
+	return (0);
+}
