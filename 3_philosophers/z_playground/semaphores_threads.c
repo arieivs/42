2353faf9c4d0773bc3f9fcae807ff25130de5235
Sara Vieira <s.cvieira@sapo.pt>
2022-02-08 00:17:11 +0000

    seg faulting, too tired, will check in the morning

--- 3_philosophers/z_playground/semaphores_threads.c
+++ 3_philosophers/z_playground/semaphores_threads.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
+#include <stdlib.h> // for rand()
 
 #define THREADS_NR 2
 #define BUFFER_SIZE 10
@@ -9,41 +10,60 @@
 
 typedef struct s_args
 {
-	pthread_mutex_t	mutexBuffer;
-	sem_t			semEmpty;
-	sem_t			semFull;
-	int				buffer[BUFFER_SIZE];
+	pthread_mutex_t	mutex_buffer;
+	sem_t			sem_empty;
+	sem_t			sem_full;
+	int				*buffer;
+	int				counter;
 }				t_args;
 
-void	*producer(void	*args)
+void	*producer(void	*undef_args)
 {
-	sem_wait
+	t_args	*args;
+
+	args = (t_args *)undef_args;
+	sem_wait(&args->sem_empty);
+	args->buffer[args->counter] = rand() % 100;
+	args->counter++;
+	sem_post(&args->sem_full);
+	return (NULL);
 }
 
-void	*consumer(void *args)
+void	*consumer(void *undef_args)
 {
+	t_args	*args;
+
+	args = (t_args *)undef_args;
+	sem_wait(&args->sem_full);
+	args->counter--;
+	printf("%d\n", args->buffer[args->counter]);
+	sem_post(&args->sem_empty);
+	return (NULL);
 }
 
 int	main(void)
 {
-	pthread_t	threads[THREADS_NR];
+	pthread_t	*threads;
 	t_args		args;
 	int			i;
 
-	pthread_mutex_init(&args.mutexBuffer, NULL);
-	sem_init(&args.semEmpty, 0, BUFFER_SIZE);
-	sem_init(&args.semFull, 0, 0);
+	threads = (pthread_t *)malloc(sizeof(pthread_t) * THREADS_NR);
+	pthread_mutex_init(&args.mutex_buffer, NULL);
+	args.sem_empty = *sem_open("nb empty places", O_CREAT, O_RDWR, BUFFER_SIZE);
+	args.sem_full = *sem_open("nb full places", O_CREAT, O_RDWR, 0);
+	args.buffer = (int *)malloc(sizeof(int) * BUFFER_SIZE);
+	args.counter = 0;
 	i = 0;
 	while (i < THREADS_NR)
 	{
 		if (i % 2 == 0)
 		{
-			if (pthread_create(&threads[i], NULL, &producer, (void *)args))
+			if (pthread_create(&threads[i], NULL, &producer, (void *)&args))
 				return (1);
 		}
 		else
 		{
-			if (pthread_create(&threads[i], NULL, &consumer, (void *)args))
+			if (pthread_create(&threads[i], NULL, &consumer, (void *)&args))
 				return (1);
 		}
 		i++;
@@ -55,6 +75,10 @@ int	main(void)
 			return (1);
 		i++;
 	}
-	// destroy mutex and semaphores
+	pthread_mutex_destroy(&args.mutex_buffer);
+	sem_close(&args.sem_empty);
+	sem_close(&args.sem_full);
+	free(args.buffer);
+	free(threads);
 	return (0);
 }
