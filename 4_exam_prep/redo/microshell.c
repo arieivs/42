fb840a3a6d5bcfc973b339a5ca4d20bbdc647a47
Sara Vieira <s.cvieira@sapo.pt>
2022-05-26 23:42:58 +0100

    another functioning microshell

--- 4_exam_prep/redo/microshell.c
+++ 4_exam_prep/redo/microshell.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <stdio.h>
+//#include <stdio.h>
 
 int	end_segment(int start, int end, char **av, char *delimiter)
 {
@@ -24,6 +24,30 @@ void	error_and_exit(void)
 {
 	write(STDERR_FILENO, "error: fatal\n", 13);
 	exit(1);
+}
+
+int	cd(char **args)
+{
+	int	nb_args = 0;
+	int	path_len = 0;
+
+	while (args[nb_args])
+		nb_args++;
+	if (nb_args == 0)
+	{
+		write(STDERR_FILENO, "error: cd: bad arguments\n", 25);
+		return (1);
+	}
+	if (chdir(args[0]) == -1)
+	{
+		write(STDERR_FILENO, "error: cd: cannot change directory to ", 38);
+		while (args[0][path_len])
+			path_len++;
+		write(STDERR_FILENO, args[0], path_len);
+		write(STDERR_FILENO, "\n", 1);
+		return (1);
+	}
+	return (0);
 }
 
 void	execute_in_child(int fd_in, int fd_out, char **args, char **envp)
@@ -63,7 +87,7 @@ int	execute_instruction(int instruction_size, char **av, char **envp)
 	while (i < instruction_size)
 	{
 		command_size = end_segment(i, instruction_size, av, "|");
-		printf("command_size %d\n", command_size);
+		//printf("command_size %d\n", command_size);
 		fd_in = fd_in_next;
 		fd_out = 1;
 		if (i + command_size < instruction_size)
@@ -73,9 +97,9 @@ int	execute_instruction(int instruction_size, char **av, char **envp)
 			fd_out = fds_pipe[1];
 			fd_in_next = fds_pipe[0];
 		}
-		printf("fd in %d, fd out %d\n", fd_in, fd_out);
+		//printf("fd in %d, fd out %d\n", fd_in, fd_out);
 		if (strcmp("cd", av[0]) == 0)
-			printf("cd\n"); //TODO
+			exit_code = cd(av + 1); // +1 to skip cd
 		else
 		{
 			pid = fork();
@@ -90,7 +114,6 @@ int	execute_instruction(int instruction_size, char **av, char **envp)
 					exit_code = WEXITSTATUS(status);
 			}
 		}
-		// execute
 		if (fd_in != 0 && close(fd_in) == -1)
 			error_and_exit();
 		if (fd_out != 1 && close(fd_out) == -1)
@@ -109,7 +132,7 @@ int main(int ac, char **av, char **envp)
 	while (i < ac)
 	{
 		instruction_size = end_segment(i, ac, av, ";");
-		printf("instruction size %d\n", instruction_size);
+		//printf("instruction size %d\n", instruction_size);
 		exit_code = execute_instruction(instruction_size, av + i, envp);
 		i = i + instruction_size + 1;
 	}
