83852c8c33457dca6ecaec0d5065b22142f51c22
Sara Vieira <s.cvieira@sapo.pt>
2022-05-26 23:30:33 +0100

    pipes and execve working

--- 4_exam_prep/redo/microshell.c
+++ 4_exam_prep/redo/microshell.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <stdio.h>
 
 int	end_segment(int start, int end, char **av, char *delimiter)
@@ -19,19 +20,84 @@ int	end_segment(int start, int end, char **av, char *delimiter)
 	return (size);
 }
 
+void	error_and_exit(void)
+{
+	write(STDERR_FILENO, "error: fatal\n", 13);
+	exit(1);
+}
+
+void	execute_in_child(int fd_in, int fd_out, char **args, char **envp)
+{
+	int	cmd_name_len = 0;
+
+	if (fd_in != 0)
+	{
+		dup2(fd_in, STDIN_FILENO);
+		close(fd_in);
+	}
+	if (fd_out != 1)
+	{
+		dup2(fd_out, STDOUT_FILENO);
+		close(fd_out);
+	}
+	execve(args[0], args, envp);
+	write(STDERR_FILENO, "error: cannot execute ", 22);
+	while (args[0][cmd_name_len])
+		cmd_name_len++;
+	write(STDERR_FILENO, args[0], cmd_name_len);
+	write(STDERR_FILENO, "\n", 1);
+}
+
 int	execute_instruction(int instruction_size, char **av, char **envp)
 {
-	int	i = 0;
-	int	command_size = 0;
-	(void)envp;
+	int		i = 0;
+	int		command_size = 0;
+	int		fd_in = 0;
+	int		fd_out = 1;
+	int		fd_in_next = 0;
+	int		fds_pipe[2];
+	pid_t	pid;
+	int		status;
+	int		exit_code = 0;
 	
 	while (i < instruction_size)
 	{
 		command_size = end_segment(i, instruction_size, av, "|");
 		printf("command_size %d\n", command_size);
+		fd_in = fd_in_next;
+		fd_out = 1;
+		if (i + command_size < instruction_size)
+		{
+			if (pipe(fds_pipe) == -1)
+				error_and_exit();
+			fd_out = fds_pipe[1];
+			fd_in_next = fds_pipe[0];
+		}
+		printf("fd in %d, fd out %d\n", fd_in, fd_out);
+		if (strcmp("cd", av[0]) == 0)
+			printf("cd\n"); //TODO
+		else
+		{
+			pid = fork();
+			if (pid == -1)
+				error_and_exit();
+			if (pid == 0)
+				execute_in_child(fd_in, fd_out, av + i, envp);
+			else
+			{
+				waitpid(pid, &status, 0);
+				if (WIFEXITED(status))
+					exit_code = WEXITSTATUS(status);
+			}
+		}
+		// execute
+		if (fd_in != 0 && close(fd_in) == -1)
+			error_and_exit();
+		if (fd_out != 1 && close(fd_out) == -1)
+			error_and_exit();
 		i = i + command_size + 1;
 	}
-	return (0);
+	return (exit_code);
 }
 
 int main(int ac, char **av, char **envp)
