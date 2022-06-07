b5b75c985ecb694f070f1a4a25768d169acd1af4
Sara Vieira <s.cvieira@sapo.pt>
2022-06-07 23:58:48 +0100

    cleaning cin so that it doesn't affect when being used in add_contact

--- 4_cpp/0_module/ex01/srcs/main.cpp
+++ 4_cpp/0_module/ex01/srcs/main.cpp
@@ -5,11 +5,14 @@ int	main(void)
 	PhoneBook	agenda;
 	std::string	user_input;
 
-	std::cout << "🤗 WELCOME TO YOUR DIGITAL PHONEBOOK! ☎️" << std::endl;
+	std::cout << "🤗 WELCOME TO YOUR DIGITAL PHONEBOOK! ☎️ " << std::endl;
 	while (true)
 	{
 		std::cout << "What do you wish to do? ADD, SEARCH or EXIT?" << std::endl;
 		std::cin >> user_input;
+		/* without cleaning the cin, it affects the result when I call it again */
+		std::cin.clear();
+		std::cin.ignore(); 
 		std::cout << std::endl;
 		if (user_input.compare("ADD") == 0)
 			agenda.add_contact();
