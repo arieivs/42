ab24bcf7ad65878a12ab73246c38ab33d3b9018b
Sara Vieira <svieira@student.42lisboa.com>
2022-06-20 16:55:52 +0100

    reorganising a bit, and reading file into string

--- 4_cpp/1_module/ex04/srcs/main.cpp
+++ 4_cpp/1_module/ex04/srcs/main.cpp
@@ -12,6 +12,53 @@ if (in_file.fail())
 
 https://cplusplus.com/reference/ios/ios_base/iostate/
 */
+int	open_or_error(std::string filename, std::ifstream *in_file, std::ofstream *out_file)
+{
+	in_file->open(filename);
+	if (in_file->fail())
+	{
+		std::cerr << "Could not open the file " << filename << std::endl;
+		return (1);
+	}
+	out_file->open(filename + ".replace");
+	if (out_file->fail())
+	{
+		std::cerr << "Could not create the replace output file" << std::endl;
+		return (1);
+	}
+	return (0);
+}
+
+int	close_or_error(std::string filename, std::ifstream *in_file, std::ofstream *out_file)
+{
+	in_file->close();
+	if (in_file->fail())
+	{
+		std::cerr << "Could not close the file " << filename << std::endl;
+		return (1);
+	}
+	out_file->close();
+	if (out_file->fail())
+	{
+		std::cerr << "Could not close the replace output file" << std::endl;
+		return (1);
+	}
+	return (0);
+}
+
+// later on it should return something...
+void	sed(std::string content, std::string s1, std::string s2)
+{
+	std::cout << content;
+	std::cout << s1 << " " << s2 << std::endl;
+}
+
+/*
+Read File into String
+"istreambuf_iterator is an input iterator that reads successive characters from the
+std::basic_streambuf object. Thus we can utilize istreambuf_iterator with an ifstream stream and read 
+the whole contents of the file into a std::string"
+*/
 int	main(int ac, char **av)
 {
 	if (ac != 4)
@@ -22,36 +69,16 @@ int	main(int ac, char **av)
 		return (1);
 	}
 
-	std::string		filename = av[1];
-	std::string		s1 = av[2];
-	std::string		s2 = av[3];
 	std::ifstream	in_file;
 	std::ofstream	out_file;
+	std::string		content;
 
-	in_file.open(filename);
-	if (in_file.fail())
-	{
-		std::cerr << "Could not open the file " << filename << std::endl;
+	if (open_or_error(av[1], &in_file, &out_file))
 		return (1);
-	}
-	out_file.open(filename + ".replace");
-	if (out_file.fail())
-	{
-		std::cerr << "Could not create the replace output file" << std::endl;
+	content = std::string(std::istreambuf_iterator<char>(in_file), std::istreambuf_iterator<char>());
+	sed(content, av[2], av[3]);
+	out_file << content; // later on the new content
+	if (close_or_error(av[1], &in_file, &out_file))
 		return (1);
-	}
-	// TODO the actual sed programme
-	in_file.close();
-	if (in_file.fail())
-	{
-		std::cerr << "Could not close the file " << filename << std::endl;
-		return (1);
-	}
-	out_file.close();
-	if (out_file.fail())
-	{
-		std::cerr << "Could not close the replace output file" << std::endl;
-		return (1);
-	}
 	return (0);
 }
