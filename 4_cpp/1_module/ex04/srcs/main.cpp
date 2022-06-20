17145e3395e5e635b6736b550b5dcdb553cc96ec
Sara Vieira <svieira@student.42lisboa.com>
2022-06-20 14:55:44 +0100

    looking into opening and closing with safety

--- 4_cpp/1_module/ex04/srcs/main.cpp
+++ 4_cpp/1_module/ex04/srcs/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <fstream>
 
+/*
+Checking if open failed
+(file doesn't exist, lack of permissions, etc...)
+in_file.open(filename);
+
+if (in_file == NULL)
+if ((in_file.rdstate() & std::ifstream::failbit) != 0)
+if (in_file.fail())
+
+https://cplusplus.com/reference/ios/ios_base/iostate/
+*/
 int	main(int ac, char **av)
 {
 	if (ac != 4)
@@ -18,15 +29,29 @@ int	main(int ac, char **av)
 	std::ofstream	out_file;
 
 	in_file.open(filename);
-	if (in_file.is_open())
+	if (in_file.fail())
 	{
-		// Nops this did not work
-		std::cerr << "File does not exit" << std::endl;
+		std::cerr << "Could not open the file " << filename << std::endl;
 		return (1);
 	}
 	out_file.open(filename + ".replace");
-	// do something
+	if (out_file.fail())
+	{
+		std::cerr << "Could not create the replace output file" << std::endl;
+		return (1);
+	}
+	// TODO the actual sed programme
 	in_file.close();
+	if (in_file.fail())
+	{
+		std::cerr << "Could not close the file " << filename << std::endl;
+		return (1);
+	}
 	out_file.close();
+	if (out_file.fail())
+	{
+		std::cerr << "Could not close the replace output file" << std::endl;
+		return (1);
+	}
 	return (0);
 }
