f1f33ab807e1ee803a45ab8ca99cddb69cdbf484
Sara Vieira <s.cvieira@sapo.pt>
2022-08-30 18:17:46 +0100

    changing form c type cast to c++ static cast 🤦‍♀️

--- 4_cpp/6_module/ex00/srcs/further_conversions.cpp
+++ 4_cpp/6_module/ex00/srcs/further_conversions.cpp
@@ -1,9 +1,9 @@
 #include "conversion.hpp"
 
 void	convert_from_char(conversions_t* conversions) {
-	conversions->n = (int)conversions->c;
-	conversions->f = (float)conversions->n;
-	conversions->d = (double)conversions->n;
+	conversions->n = static_cast<int>(conversions->c);
+	conversions->f = static_cast<float>(conversions->n);
+	conversions->d = static_cast<double>(conversions->n);
 }
 
 void	convert_from_int(conversions_t* conversions) {
@@ -14,46 +14,46 @@ void	convert_from_int(conversions_t* conversions) {
 		conversions->d = (1.0/0.0);
 		return ;
 	}
-	conversions->c = (char)conversions->n;
-	conversions->f = (float)conversions->n;
-	conversions->d = (double)conversions->n;
+	conversions->c = static_cast<char>(conversions->n);
+	conversions->f = static_cast<float>(conversions->n);
+	conversions->d = static_cast<double>(conversions->n);
 }
 
 void	convert_from_float(conversions_t* conversions) {
 	long int	long_n;
 
-	conversions->d = (double)conversions->f;
+	conversions->d = static_cast<double>(conversions->f);
 	if (conversions->is_pseudo_literal) {
 		conversions->is_char_impossible = true;
 		conversions->is_int_impossible = true;
 		return ;
 	}
-	conversions->n = (int)conversions->f;
-	long_n = (long int)conversions->f;
+	conversions->n = static_cast<int>(conversions->f);
+	long_n = static_cast<long int>(conversions->f);
 	if (conversions->n != long_n) {
 		conversions->is_int_impossible = true;
 		conversions->is_char_impossible = true;
 		return ;
 	}
-	conversions->c = (char)conversions->n;
+	conversions->c = static_cast<char>(conversions->n);
 }
 
 void	convert_from_double(conversions_t* conversions) {
 	long int	long_n;
 
-	conversions->f = (float)conversions->d;
+	conversions->f = static_cast<float>(conversions->d);
 	if (conversions->is_pseudo_literal) {
 		conversions->is_char_impossible = true;
 		conversions->is_int_impossible = true;
 		return ;
 	}
-	conversions->n = (int)conversions->d;
-	long_n = (long int)conversions->d;
+	conversions->n = static_cast<int>(conversions->d);
+	long_n = static_cast<long int>(conversions->d);
 	if (conversions->n != long_n) {
 		conversions->is_int_impossible = true;
 		conversions->is_char_impossible = true;
 		return ;
 	}
-	conversions->c = (char)conversions->n;
+	conversions->c = static_cast<char>(conversions->n);
 }	
 
