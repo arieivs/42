2384068c31054d6aea768e226db8f0ec39e5184e
Sara Vieira <svieira@student.42lisboa.com>
2022-09-06 17:14:55 +0100

    finding numbers

---
+++ 4_cpp/8_module/ex00/easyfind.hpp
@@ -0,0 +1,27 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm>
+
+class IntegerNotFound : public std::exception {
+    public:
+        virtual const char* what() const throw() {
+            return "Integer not found in given container";
+        }
+};
+
+/* T is a container of ints */
+template<typename T>
+int easyfind(T container, int n) {
+    typename T::iterator    it;
+    typename T::iterator    it_end = container.end();
+
+    it = std::find(container.begin(), it_end, n);
+    if (it == it_end) {
+        throw IntegerNotFound();
+        return (-1);
+    }
+    return (*it);
+}
+
+#endif
---
+++ 4_cpp/8_module/ex00/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include "easyfind.hpp"
+
+int main(void) {
+    int                 arr[] = {2, 4, 6, 8};
+    size_t              arr_len = (sizeof(arr) / sizeof(int));
+    std::vector<int>    vect(arr, arr + arr_len);
+    int                 found;
+
+    try {
+        found = easyfind(vect, 4);
+        std::cout << "Found number " << found << std::endl;
+    }
+    catch (IntegerNotFound& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    try {
+        found = easyfind(vect, 5);
+        std::cout << "Found number " << found << std::endl;
+    }
+    catch (IntegerNotFound& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    return (0);
+}
