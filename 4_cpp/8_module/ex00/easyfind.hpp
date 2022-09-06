2f0bf4c00e8077db0542a9a1f6e9ce2880bd1747
Sara Vieira <svieira@student.42lisboa.com>
2022-09-06 17:53:08 +0100

    replacing spaces for tabs

--- 4_cpp/8_module/ex00/easyfind.hpp
+++ 4_cpp/8_module/ex00/easyfind.hpp
@@ -4,24 +4,24 @@
 # include <algorithm>
 
 class IntegerNotFound : public std::exception {
-    public:
-        virtual const char* what() const throw() {
-            return "Integer not found in given container";
-        }
+	public:
+		virtual const char* what() const throw() {
+			return "Integer not found in given container";
+		}
 };
 
 /* T is a container of ints */
 template<typename T>
 int easyfind(T container, int n) {
-    typename T::iterator    it;
-    typename T::iterator    it_end = container.end();
+	typename T::iterator	it;
+	typename T::iterator	it_end = container.end();
 
-    it = std::find(container.begin(), it_end, n);
-    if (it == it_end) {
-        throw IntegerNotFound();
-        return (-1);
-    }
-    return (*it);
+	it = std::find(container.begin(), it_end, n);
+	if (it == it_end) {
+		throw IntegerNotFound();
+		return (-1);
+	}
+	return (*it);
 }
 
 #endif
--- 4_cpp/8_module/ex00/main.cpp
+++ 4_cpp/8_module/ex00/main.cpp
@@ -3,26 +3,26 @@
 #include "easyfind.hpp"
 
 int main(void) {
-    int                 arr[] = {2, 4, 6, 8};
-    size_t              arr_len = (sizeof(arr) / sizeof(int));
-    std::vector<int>    vect(arr, arr + arr_len);
-    int                 found;
+	int					arr[] = {2, 4, 6, 8};
+	size_t				arr_len = (sizeof(arr) / sizeof(int));
+	std::vector<int>	vect(arr, arr + arr_len);
+	int					found;
 
-    try {
-        found = easyfind(vect, 4);
-        std::cout << "Found number " << found << std::endl;
-    }
-    catch (IntegerNotFound& e) {
-        std::cout << e.what() << std::endl;
-    }
+	try {
+		found = easyfind(vect, 4);
+		std::cout << "Found number " << found << std::endl;
+	}
+	catch (IntegerNotFound& e) {
+		std::cout << e.what() << std::endl;
+	}
 
-    try {
-        found = easyfind(vect, 5);
-        std::cout << "Found number " << found << std::endl;
-    }
-    catch (IntegerNotFound& e) {
-        std::cout << e.what() << std::endl;
-    }
+	try {
+		found = easyfind(vect, 5);
+		std::cout << "Found number " << found << std::endl;
+	}
+	catch (IntegerNotFound& e) {
+		std::cout << e.what() << std::endl;
+	}
 
-    return (0);
+	return (0);
 }
