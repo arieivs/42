3f084f699e4bd216963139b8214944fae8622280
Sara Vieira <svieira@student.42lisboa.com>
2022-09-08 17:58:53 +0100

    fixed iter

--- 4_cpp/7_module/ex01/iter.hpp
+++ 4_cpp/7_module/ex01/iter.hpp
@@ -1,9 +1,31 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+/* normal function */
 template<typename T>
 void	iter(T* arr, int len, void(*f)(T&)) {
 	int	i;
+
+	for (i = 0; i < len; i++)
+		f(arr[i]);
+}
+
+/* function with const parameter
+ * use std::size_t instead of int for len to avoid ambiguity
+ * between function calls
+ */
+template<typename T>
+void	iter(T* arr, std::size_t len, void(*f)(T const &)) {
+	std::size_t	i;
+
+	for (i = 0; i < len; i++)
+		f(arr[i]);
+}
+
+/* template function */
+template<typename T, typename F>
+void	iter(T* arr, std::size_t len, F f) {
+	std::size_t	i;
 
 	for (i = 0; i < len; i++)
 		f(arr[i]);
--- 4_cpp/7_module/ex01/main.cpp
+++ 4_cpp/7_module/ex01/main.cpp
@@ -15,11 +15,38 @@ void	print_louder(std::string& str) {
 	std::cout << "!" << std::endl;
 }
 
+/* EVAL SHEET */
+class Awesome
+{
+	public:
+		Awesome( void ) : _n( 42 ) { return; }
+		int get( void ) const { return this->_n; }
+	private:
+		int _n;
+};
+
+std::ostream & operator<<(std::ostream & out, Awesome const & a) {
+	out << a.get();
+	return (out);
+}
+
+template<typename T>
+void print( T const & x ) { std::cout << x << std::endl; return; }
+
 int	main(void) {
 	int			arr_ints[] = {1, 2, 3, 4, 5};
 	std::string	arr_strs[] = {"Hello darkness", "My old friend"};
 
 	iter(arr_ints, 5, &print_double);
 	iter(arr_strs, 2, &print_louder);
+
+	/* EVAL SHEET */
+	const int tab[] = { 0, 1, 2, 3, 4 };
+	const Awesome tab2[] = { Awesome(), Awesome(), Awesome(), Awesome(), Awesome()};
+	iter( tab, 5, print );
+	iter( tab2, 5, print );
+
+	// TODO find how to test template function
+
 	return (0);
 }
