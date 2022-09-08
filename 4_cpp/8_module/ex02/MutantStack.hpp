24b3101fc88035955b744602a9ebfd9a6d2e0750
Sara Vieira <svieira@student.42lisboa.com>
2022-09-08 12:14:17 +0100

    getting somewhere, end() not working

--- 4_cpp/8_module/ex02/MutantStack.hpp
+++ 4_cpp/8_module/ex02/MutantStack.hpp
@@ -6,59 +6,79 @@
 // these classes also need to be orthodox?
 // https://en.cppreference.com/w/cpp/iterator/iterator
 // https://cplusplus.com/reference/iterator/iterator/
+// https://www.learncpp.com/cpp-tutorial/stl-iterators-overview/
 template<typename T>
 class MutantStack : public std::stack<T> {
 	public:
+		// needs to be orthodox right?
+
 		class iterator : public std::iterator<std::bidirectional_iterator_tag,
-											  std::stack<T>,
-											  std::stack<T>,
-											  std::stack<T>*,
-											  std::stack<T>& > {
+											  MutantStack<T>,
+											  MutantStack<T>,
+											  MutantStack<T>*,
+											  MutantStack<T>& > {
 			private:
 				std::stack<T>	_stack;
+				std::stack<T>	_backwards_stack;
 			public:
-				// entao... preciso de qq coisa... mas o q? uma stack?
-				// long num = FROM; (private)
-				// why explicit?
-				explicit iterator(std::stack<T> stack) : _stack(stack) {} // constructor
-				iterator(iterator const & src) : _stack(src._stack) {}
-				iterator&	operator++() {
-					++_stack; // not going to work
+				iterator(void) : _stack(), _backwards_stack() {}
+				iterator(MutantStack<T> mstack) : _stack(mstack), _backwards_stack() {}
+				iterator(iterator const & src) : _stack(src._stack),
+												 _backwards_stack(src._backwards_stack) {}
+				iterator&	operator=(iterator const & src) {
+					_stack = src._stack;
+					_backwards_stack = src._backwards_stack;
 					return (*this);
 				}
-				iterator	operator++(std::stack<T>) {
-					iterator previous = *this;
-					++(*this); // probably won't work either
-					return previous;
+				~iterator(void) {}
+
+				iterator&	operator++() {
+					T	element = _stack.top();
+
+					_stack.pop();
+					_backwards_stack.push(element);
+					return (*this);
 				}
-				// missing --
+				iterator	operator++(int) {
+					iterator	previous = *this;
+					T	element = _stack.top();
+
+					_stack.pop();
+					_backwards_stack.push(element);
+					return (previous);
+				}
+				iterator&	operator--() {
+					T	element = _backwards_stack.top();
+
+					_backwards_stack.pop();
+					_stack.push(element);
+					return (*this);
+				}
+				iterator	operator--(int) {
+					iterator	previous = *this;
+					T	element = _backwards_stack.top();
+
+					_backwards_stack.pop();
+					_stack.push(element);
+					return (previous);
+				}
 				bool		operator==(iterator other) const {
-					return _stack == other._stack;
+					return (_stack == other._stack && _backwards_stack == other._backwards_stack);
 				}
 				bool		operator!=(iterator other) const {
-					return _stack != other._stack;
+					return (_stack != other._stack || _backwards_stack != other._backwards_stack);
 				}
-				std::stack<T>&	operator*() const {
-					return _stack;
+				T const &	operator*() const {
+					return _stack.top();
 				}
-
-				/*iterator&	operator++() {num = Treturn _stack.top();
-				// int* p; (private)
-				MyIterator(int* x) :p(x) {} // constructor
-				MyIterator(const MyIterator& mit) : p(mit.p) {} // copy constructor
-				MyIterator& operator++() {++p;return *this;}
-				MyIterator operator++(int) {MyIterator tmp(*this); operator++(); return tmp;}
-				bool operator==(const MyIterator& rhs) const {return p==rhs.p;}
-				bool operator!=(const MyIterator& rhs) const {return p!=rhs.p;}
-				int& operator*() {return *p;}*/
 		};
 
 		iterator	begin(void) {
-			return _stack.top();
+			return iterator(*this);
 		}
 		iterator	end(void) {
-			return _stack.top();
-		} // ah pois... como? temporario. acho que preciso de duas stacks...
+			return iterator(*(this + this->size())); // seg fault... but it should be end, no?
+		}
 };
 
 #endif
--- 4_cpp/8_module/ex02/main.cpp
+++ 4_cpp/8_module/ex02/main.cpp
@@ -3,54 +3,81 @@
 #include <iostream>
 
 int main(void) {
-	{
-		std::cout << "MUTANT STACK" << std::endl;
-		MutantStack<int>	mstack;
+	MutantStack<int>	mstack;
+	std::list<int>		lst;
 
-		mstack.push(5);
-		mstack.push(17);
-		std::cout << mstack.top() << std::endl;
-		mstack.pop();
-		std::cout << mstack.size() << std::endl;
-		mstack.push(3);
-		mstack.push(5);
-		mstack.push(737);
-		//[...]
-		mstack.push(0);
-		MutantStack<int>::iterator it = mstack.begin();
-		MutantStack<int>::iterator ite = mstack.end();
-		++it;
-		--it;
-		while (it != ite) {
-			std::cout << *it << std::endl;
-			++it;
-		}
-		std::stack<int> s(mstack);
-	}
-	{
-		std::cout << std::endl << "STD::LIST" << std::endl;
-		std::list<int>	lst;
+	std::cout << "STACK REGULAR OPERATIONS" << std::endl;
+	std::cout << "Mutant Stack" << std::endl;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << "First out: " << mstack.top() << " (should be 17)" << std::endl;
+	mstack.pop();
+	std::cout << "Size: " << mstack.size() << " (should be 1)" << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	std::cout << std::endl << "vs List" << std::endl;
+	lst.push_front(5);
+	lst.push_front(17);
+	std::cout << "First out: " << lst.front() << " (should be 17)" << std::endl;
+	lst.pop_front();
+	std::cout << "Size: " << lst.size() << " (should be 1)" << std::endl;
+	lst.push_front(3);
+	lst.push_front(5);
+	lst.push_front(737);
+	lst.push_front(0);
 
-		lst.push_back(5);
-		lst.push_back(17);
-		std::cout << lst.back() << std::endl;
-		lst.pop_back();
-		std::cout << lst.size() << std::endl;
-		lst.push_back(3);
-		lst.push_back(5);
-		lst.push_back(737);
-		//[...]
-		lst.push_back(0);
-		/*std::list<int>::iterator it = lst.begin();
-		std::list<int>::iterator ite = lst.end();
+	std::cout << std::endl << "ITERATORS - BEGIN, IN/DECREMENT AND DEREFERENCE" << std::endl;
+	std::cout << "Mutant Stack" << std::endl;
+	MutantStack<int>::iterator it = mstack.begin();
+	++it;
+	std::cout << *it << " ";
+	it++;
+	std::cout << *it << " ";
+	--it;
+	std::cout << *it << " ";
+	it--;
+	std::cout << *it << std::endl;
+	std::cout << std::endl << "vs List" << std::endl;
+	std::list<int>::iterator lit = lst.begin();
+	++lit;
+	std::cout << *lit << " ";
+	lit++;
+	std::cout << *lit << " ";
+	--lit;
+	std::cout << *lit << " ";
+	lit--;
+	std::cout << *lit << std::endl;
+
+	std::cout << std::endl << "ITERATORS - COPYING AND COMPARING" << std::endl;
+	MutantStack<int>::iterator it2(it);
+	MutantStack<int>::iterator it3;
+
+	it3 = it;
+	if (it == it2 && it == it3)
+		std::cout << "All good!" << std::endl;
+	else
+		std::cout <<"Something went wrong..." << std::endl;
+	
+	std::cout << std::endl << "ITERATORS - END AND LOOPS" << std::endl;
+	std::cout << "Mutant Stack" << std::endl;
+	/*MutantStack<int>::iterator ite = mstack.end();
+	while (it != ite) {
+		std::cout << *it << " ";
 		++it;
-		--it;
-		while (it != ite)
-		{
-		std::cout << *it << std::endl;
-		++it;
-		}
-		std::stack<int> s(lst);*/
 	}
+	std::cout << std::endl;*/
+	std::cout << std::endl << "vs List" << std::endl;
+	std::list<int>::iterator lite = lst.end();
+	while (lit != lite) {
+		std::cout << *lit << " ";
+		++lit;
+	}
+	std::cout << std::endl;
+
+	std::cout << std::endl << "CREATING A STACK FROM A MUTANT STACK" << std::endl;
+	std::stack<int> s(mstack);
+	std::cout << "If I'm being printed, everything went fine" << std::endl << std::endl;
 	return 0;
 }
