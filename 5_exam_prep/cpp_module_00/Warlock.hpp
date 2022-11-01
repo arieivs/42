2b21d2ec1fc40685056fa82ad228f565b71d5297
Sara Vieira <s.cvieira@sapo.pt>
2022-11-01 18:49:21 +0000

    module 0 done

--- 5_exam_prep/cpp_module_00/Warlock.hpp
+++ 5_exam_prep/cpp_module_00/Warlock.hpp
@@ -12,22 +12,28 @@
  *
  * However, our Warlock cannot be:
  * - instantiated without name and title => no default constructor
- * - instatiated by copy => no copy constructor
- * - able to be copied => no opeartor= overload
+ * - instatiated by copy => private copy constructor (simply not having it,
+ *   won't work!!)
+ * - able to be copied => private opeartor= overload (simply not having it,
+ *   won't work!!)
 */
 
 class Warlock {
-    public:
-        Warlock(std::string name, std::string title);
-        ~Warlock(void);
+	public:
+		Warlock(std::string name, std::string title);
+		~Warlock(void);
 
-        std::string const & getName(void) const;
-        std::string const & getTitle(void) const;
-        void                setTitle(std::string const & new_title);
-    
-    private:
-        std::string name;
-        std::string title;
+		std::string const &	getName(void) const;
+		std::string const &	getTitle(void) const;
+		void				setTitle(std::string const & new_title);
+		void				introduce() const;
+
+	private:
+		std::string	name;
+		std::string	title;
+
+		Warlock(Warlock const & src);
+		Warlock const &	operator=(Warlock const & src);
 };
 
 #endif
